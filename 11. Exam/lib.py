a0d981db32a054e61d5bd47af2074e78de741d24
DEV <dev@step.rivne>
2023-08-21 11:54:15 +0300

    Fix menu

--- 11. Exam/lib.py
+++ 11. Exam/lib.py
@@ -3,6 +3,8 @@
 
 
 import csv
+import time
+import os
 
 def read_file():
     try:
@@ -25,6 +27,7 @@ def write_file(database):
                 file.writelines(f"{item},")
             file.writelines("\n")
         file.close()
+        return print("File saved successfully!")
     except Exception as error:
         print(f"Error: {error}")
 
@@ -35,17 +38,50 @@ csvreader = read_file()
 
 def create_database(csvreader, database):
     for item in csvreader:
+        item[3] = int(item[3]) # Change data type for price
         database.append(item)
 
 
+def waiting():
+    bar = [
+    "[        ]",
+    "[=       ]",
+    "[===     ]",
+    "[====    ]",
+    "[=====   ]",
+    "[======  ]",
+    "[======= ]",
+    "[========]",
+    "[ =======]",
+    "[  ======]",
+    "[   =====]",
+    "[    ====]",
+    "[     ===]",
+    "[      ==]",
+    "[       =]",
+    "[        ]",
+    "[        ]"
+    ]
+    i = 0
+
+    while i <= 50:
+        print(f"\t    {bar[i % len(bar)]}", end="\r")
+        time.sleep(0.05)
+        i += 1
+    
+
 def menu():
     create_database(csvreader, database)
-    while True:
-        print("1. Add item\n2. Print items\n3. Edit item\n4. Remove item\n5. Search item\n\n0. Exit\n")
-        choice = input("Enter a choice: ")
+    exit = False
+    while not exit:
+        print("----------------------------------")
+        print("\t- 1. Add item -\n\t- 2. Print items -\n\t- 3. Edit item -\n\t- 4. Remove item -\n\t- 5. Search item -\n\n\t- 0. Exit -\n")
+        choice = input(">> ")
+        print("----------------------------------")
         if choice == "1":
             add_item(database)
         elif choice == "2":
+            waiting()
             print_all_items(database)
         elif choice == "3":
             edit_item(database)
@@ -54,8 +90,9 @@ def menu():
         elif choice == "5":
             search_item(database)
         elif choice == "0":
-            break
-        write_file(database)
+            exit = True
+    
+    write_file(database)
     
 
 def add_item(database):
@@ -65,7 +102,7 @@ def add_item(database):
     number = str(i+1)
     name = input("Enter an item name: ")
     producer = input("Enter an item producer: ")
-    price = input("Enter an item price: ")
+    price = int(input("Enter an item price: "))
     item = [number, name, producer, price]
     database.append(item)
 
@@ -110,12 +147,25 @@ def search_item(database):
     search_type = input("1. Search by name\n2. Search by producer\n3. Search by price\n\nEnter a type of search: ")
     i = 1
     if search_type == "1":
-        search_name = input("Enter a name for search: ")
-        print("\n==================\nRESULT: ")
+        search_name = input("Enter a name to search: ")
+        print("\nRESULT: ")
         for item in database:
             if search_name in item:
-                print(f"{i}. Name: {item[0]}\nProducer: {item[1]}\nPrice: {item[2]}\n")
+                print_item(item)
             i+=1
-    
+    elif search_type == "2":
+        search_producer = input("Enter a producer to search: ")
+        print("\nRESULT: ")
+        for item in database:
+            if search_producer in item:
+                print_item(item)
+            i+=1
+    elif search_type == "3":
+        start = int(input("Enter a start of range: "))
+        finish = int(input("Enter a finish of range: "))
+        print("\nRESULT: ")
+        for item in database:
+            if item[3] >= start and item[3] <= finish:
+                print_item(item)
 
 
