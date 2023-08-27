19ca2bca483e530a1e71f1a5f6055c2edb3b5a0d
Kostia <kostianikolaiev145@gmail.com>
2023-08-27 17:47:18 +0300

    Update exam project

--- 11. Exam/lib.py
+++ 11. Exam/lib.py
@@ -5,7 +5,7 @@
 import csv
 import time
 import os
-from datetime import date
+from datetime import date, timedelta, datetime
 from tabulate import tabulate
 
 
@@ -41,7 +41,7 @@ csvreader = read_file()
 
 def create_database(csvreader, database):
     for item in csvreader:
-        item[3] = int(item[3]) # Change price data type
+        item[3] = int(item[3])  # Change price data type
         database.append(item)
 
 
@@ -51,7 +51,9 @@ def menu():
     while not exit:
         numbering(database)
         print("\n. . .")
-        print("\t- 1. Add item -\n\t- 2. Print items -\n\t- 3. Edit item -\n\t- 4. Remove item -\n\t- 5. Search item -\n\n\t- 0. Exit -\n")
+        print(
+            "\t- 1. Add item\n\t- 2. Print items\n\t- 3. Edit item\n\t- 4. Remove item\n\t- 5. Search item\n\n\t- 0. Exit\n"
+        )
         choice = input(">> ")
         if choice == "1":
             add_item(database)
@@ -59,42 +61,75 @@ def menu():
             print_database(database)
         elif choice == "3":
             print_database(database)
-            search = input("Enter a number of item to edit: ")
-            edit_item(database, search)
+            edit = int(input("Enter a number of item to edit: "))
+            edit_item(database, edit)
         elif choice == "4":
-            # It's going to be print and search!!
-            remove_item(database)
+            print_database(database)
+            delete = int(input("Enter a number of item to delete: "))
+            remove_item(database, delete)
         elif choice == "5":
-            search_item(database)
+            search = input(
+                "1. Search by name\n2. Search by producer\n3. Search by price\n\nEnter a type of search: "
+            )
+            search_item(database, search)
         elif choice == "0":
             exit = True
-    
+
     write_file(database)
-    
+
 
 def add_item(database):
     i = 0
     for item in database:
-        i+=1
-    number = str(i+1)
+        i += 1
+    number = str(i + 1)
     name = input("Enter an item name: ")
     producer = input("Enter an item producer: ")
     price = int(input("Enter an item price: "))
     product_group = input("Enter an item product group: ")
     come_date = date.today()
-    life_term = input("Enter an item life term: ")
+    life_term_input = input("Enter an item life term: ")
+    life_term_input = life_term_input.split()
+    life_term_number = int(life_term_input[0])
+    if life_term_input[1] == "day" or life_term_input[1] == "days":
+        life_term = come_date
+        life_term = life_term + timedelta(days=life_term_number)
+    if life_term_input[1] == "week" or life_term_input[1] == "weeks":
+        life_term = come_date
+        life_term = life_term + timedelta(weeks=life_term_number)
+    if life_term_input[1] == "month" or life_term_input[1] == "months":
+        life_term_number = life_term_number * 30
+        life_term = come_date
+        for i in range(life_term_number):
+            life_term = life_term + timedelta(days=1)
+
     item = [number, name, producer, price, product_group, come_date, life_term]
     database.append(item)
 
 
 def print_database(item):
-    print(tabulate(item, headers=["№", "Name", "Producer", "Price", "Group", "Come date", "Life term"]))
+    print(
+        tabulate(
+            item,
+            headers=[
+                "№",
+                "Name",
+                "Producer",
+                "Price",
+                "Group",
+                "Come date",
+                "Life term",
+            ],
+        )
+    )
 
 
 def edit_item(database, choice):
     for item in database:
         if item[0] == choice:
-            editing = input("1. Name\n2. Producer\n3. Price\n\nEnter an element to edit: ")
+            editing = input(
+                "1. Name\n2. Producer\n3. Price\n\nEnter an element to edit: "
+            )
             if editing == "1":
                 new_name = input("Enter a new name: ")
                 item[1] = new_name
@@ -106,35 +141,25 @@ def edit_item(database, choice):
                 item[3] = new_price
 
 
-def remove_item(database):
-    print_database(database)
-    deleting = int(input("Enter a number of item to remove: "))
+def remove_item(database, choice):
     i = 1
     for item in database:
-        if i == deleting:
+        if i == choice:
             database.remove(item)
-        i+=1
+        i += 1
 
 
 def numbering(database):
     i = 1
     for item in database:
         item[0] = i
-        i+=1
+        i += 1
 
 
-def search_by_name(database, search_name):
+def search(database, search_item):
     search_list = []
     for item in database:
-            if search_name in item:
-                search_list.append(item)
-    return search_list
-
-
-def search_by_producer(database, search_producer):
-    search_list = []
-    for item in database:
-        if search_producer in item:
+        if search_item in item:
             search_list.append(item)
     return search_list
 
@@ -143,24 +168,21 @@ def search_by_price(database, start, finish):
     search_list = []
     for item in database:
         if item[3] >= start and item[3] <= finish:
-             search_list.append(item)
+            search_list.append(item)
     return search_list
 
 
-def search_item(database):
-    search_type = input("1. Search by name\n2. Search by producer\n3. Search by price\n\nEnter a type of search: ")
-    if search_type == "1":
+def search_item(database, choice):
+    if choice == "1":
         search_name = input("Enter a name to search: ")
         print(f"\n*** Search by Name '{search_name}' - RESULT: ")
-        print_database(search_by_name(database, search_name))
-    elif search_type == "2":
+        print_database(search(database, search_name))
+    elif choice == "2":
         search_producer = input("Enter a producer to search: ")
         print(f"\n*** Search by Producer '{search_producer}' - RESULT: ")
-        print_database(search_by_producer(database, search_producer))
-    elif search_type == "3":
+        print_database(search(database, search_producer))
+    elif choice == "3":
         start = int(input("Enter a start of range: "))
         finish = int(input("Enter a finish of range: "))
         print(f"\n*** Search by Price '{start} - {finish}' - RESULT: ")
         print_database(search_by_price(database, start, finish))
-
-
